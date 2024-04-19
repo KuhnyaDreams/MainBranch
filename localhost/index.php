40b9d7ffc3c8c7a9f4d505f4a7a73408f68622c0
Wobir <159927654+Wobir@users.noreply.github.com>
2024-04-19 11:07:15 +0500

    Add files via upload
    Обновил свойства карты, теперь не показываются стандартные настройки управления. Заложил основу интерфейса(Два плейсхолдера с разных сторон экрана Xb ).

--- localhost/index.php
+++ localhost/index.php
@@ -5,24 +5,35 @@
         <script src="https://api-maps.yandex.ru/2.1/?apikey=c472441-ed32-4bed-a362-addea55b0252&lang=ru_RU" type="text/javascript">
         </script>
         <link href="./css/styles.css" rel="stylesheet">
-        <script src=".js/jquery.js" ></script>
+        <script src="./js/jquery.js" ></script>
     </head>
 
     <body>
         <div class="maxMap" id="map"></div>
-        <d>
+        <div class="interface" id="interface">
+            <div>
+                <img class="menu" src="img/placeholder.svg">
+            </div>
+            <div>
+                <img class="account" src="img/placeholder.svg">
+            </div>
+        </div>
     </body>
+
     <script type="text/javascript">
         ymaps.ready(init);
 
         var myMap,
-            fullScreen = false;
+            fullScreen = true;
 
         function init () {
             myMap = new ymaps.Map('map', {
                 center: [56.83745,60.59765],
-                zoom: 14
+                zoom: 14,
+                controls: []
             });
         }
+        myMap.container.fitToViewport();
     </script>
+
 </html>
