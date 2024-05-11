4842dff2e4b563a1684fa7a47afbcef6fe3615c0
Wobir <159927654+Wobir@users.noreply.github.com>
2024-05-11 21:32:30 +0500

    Add files via upload
    небольшой фикс прошлого коммита

--- localhost/js/profileButtons.js
+++ localhost/js/profileButtons.js
@@ -2,17 +2,17 @@ let buttons = document.querySelectorAll('.profile-item');
 
 for (let b of buttons){
     b.onclick = function(){
-        if (b.className==('profile-item active'))
+        if (b.querySelector('span').className==('active'))
         {
-            b.classList.remove('active');
+            b.querySelector('span').classList.remove('active');
             document.querySelector('.'+b.id).classList.add('not-visible');
         }
         else{
             for (let b1 of buttons){
-                b1.classList.remove('active');
+                b1.querySelector('span').classList.remove('active');
                 document.querySelector('.'+b1.id).classList.add('not-visible');
             }
-            b.classList.add('active');
+            b.querySelector('span').classList.add('active');
             document.querySelector('.'+b.id).classList.remove('not-visible');
         }
     }
